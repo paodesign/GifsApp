3ea98b903b2106d39b642082300794af14ba3968
Paola Medina <paaaomedina@gmail.com>
2022-08-22 21:10:26 -0300

    App finalizada

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../intesface/gifs.interface';
 
@@ -8,6 +8,7 @@ import { Gif, SearchGifsResponse } from '../intesface/gifs.interface';
 export class GifsService {
 
   private apiKey: string = 'wZ5iEuR5ZXeVPOR4wdCLwTwfk12UE820';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
   public resultados:Gif[] = [];
@@ -21,6 +22,7 @@ export class GifsService {
 
     if( localStorage.getItem('historial') ){
       this._historial = JSON.parse( localStorage.getItem('historial')!) || [];
+      this.resultados = JSON.parse( localStorage.getItem('resultados')!) || [];
     }
    
 
@@ -38,13 +40,18 @@ export class GifsService {
 
   }
 
-  this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=wZ5iEuR5ZXeVPOR4wdCLwTwfk12UE820&q=${query}&limit=10`)
+  const params = new HttpParams()
+        .set('api_key',this.apiKey )
+        .set('limit', '10')
+        .set('q', query );
+  console.log(params.toString());
+
+  this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params })
       .subscribe( ( resp ) => {
         console.log( resp.data );
         this.resultados = resp.data;
-       
+        localStorage.setItem('resultados', JSON.stringify(this.resultados) );
       });
 
-  console.log( this._historial )
  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -12,6 +12,8 @@ export class SidebarComponent  {
   }
   constructor( private gifsService: GifsService ) {}
   
-
+  buscar( termino: string ){
+   this.gifsService.buscarGifs(termino);
+  }
 
 }
